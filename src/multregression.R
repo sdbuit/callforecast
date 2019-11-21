36c310a9d69d84ec10a36793a6b5de4e8cbc361a
Samuel <samuelbuit@mail.weber.edu>
2019-11-21 16:36:23 -0700

    update

--- src/multregression.R
+++ src/multregression.R
@@ -1,27 +1,29 @@
 url <- "https://raw.githubusercontent.com/sdbuit/callforecast/master/data/interim/02training.csv"
 df <- read.table(url, sep = ",", header = T)[,-1]
 
+# head(df)
+
 #set.seed(1)
-train = sample(1:nrow(df), floor(.7*nrow(df)), replace=FALSE)
-lm <- lm(CV ~ as.factor(time24) + as.factor(day), data=df, subset=train)
+# train = sample(1:nrow(df), floor(.7*nrow(df)), replace=FALSE)
+# lm <- lm(CV ~ as.factor(time24) + as.factor(day), data=df, subset=train)
 
-error <- rep(NA, 500)    
-for (i in c(1:500)) {
-    train = sample(1:nrow(df), floor(.7*nrow(df)), replace=FALSE)
-    test <- df[-train, "CV"]
-    yhat <- predict(lm, newdata = df[-train,])
-    error[i] <- mean((yhat - test)^2)
-    #plot(yhat,test)
-}
-cat('Model (test 1) MSE:\t', round(mean(error), 2))
+# error <- rep(NA, 500)    
+# for (i in c(1:500)) {
+#     train = sample(1:nrow(df), floor(.7*nrow(df)), replace=FALSE)
+#     test <- df[-train, "CV"]
+#     yhat <- predict(lm, newdata = df[-train,])
+#     error[i] <- mean((yhat - test)^2)
+#     #plot(yhat,test)
+# }
+# cat('Model (test 1) MSE:\t', round(mean(error), 2))
 
-error <- rep(NA, 500)    
-for (i in c(1:500)) {
-    n = nrow(df)
-    train <- sample(1:n, 1300)
-    error[i] <- mean(((df$CV-predict(lm,df))[-train])^2)
-}
-cat('\nModel (test 2) MSE:\t', round(mean(error), 2))
+# error <- rep(NA, 500)    
+# for (i in c(1:500)) {
+#     n = nrow(df)
+#     train <- sample(1:n, 1300)
+#     error[i] <- mean(((df$CV-predict(lm,df))[-train])^2)
+# }
+# cat('\nModel (test 2) MSE:\t', round(mean(error), 2))
 
 # library(boot)
 # glm.fit <- glm(CV ~ as.factor(time24) + as.factor(day), data=df)
